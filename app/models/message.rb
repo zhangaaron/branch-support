99724fb782260452ef4ba7bf80dd9b610eccab44
Aaron Zhang <a_zhang@apple.com>
2017-08-27 15:09:17 -0700

    Add message scopes for filtering and sorting

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
   has_many :replies
+
+  scope :not_resolved, -> {where(resolved: false)}
+  scope :order_by_priority, -> {order(priority: :asc)}
+  scope :order_by_most_recent, -> {order(created_at: :desc )}
 end
