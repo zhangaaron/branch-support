e72329d43c2acd7722069f583ccf42eabada94e1
Aaron Zhang <a_zhang@apple.com>
2017-09-08 10:42:12 -0700

    Add uncommited files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  skip_before_filter :logged_in?
+  def new
+
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+    if user
+      session['user'] = user.id
+      flash[:notice] = "Logged in as: #{params[:session][:email]}"
+      flash.keep
+      redirect_to root_path
+    else
+      flash[:notice] = "Couldn't find a user with value: #{params[:session][:email]}"
+      flash.keep
+      redirect_to login_path
+    end
+
+  end
+
+  def destroy
+    session.clear
+  end
+end
